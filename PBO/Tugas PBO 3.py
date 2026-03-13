2df70de6f1c23ef045a9c2dc29e9b8fcfa969e51
MIrfandi12550111460 <12550111460@students.uin-suska.ac.id>
2026-03-13 13:05:05 +0700

    Add files via upload

--- PBO/Tugas PBO 3.py
+++ PBO/Tugas PBO 3.py
@@ -5,6 +5,7 @@ class User:
         self.age = age
         self.email = email
         self.city = city
+        self.login_attempts = 0
 
     def describe_user(self):
         print("Nama Depan :", self.first_name)
@@ -18,15 +19,34 @@ class User:
         print("Halo", self.first_name, self.last_name, "! Selamat datang.")
         print()
 
+    def increment_login_attempts(self):
+        self.login_attempts += 1
+
+    def reset_login_attempts(self):
+        self.login_attempts = 0
+
+
 user1 = User("Muhamad", "Irfandi", 20, "irfandi@email.com", "Pekanbaru")
-user2 = User("Andi", "Saputra", 22, "andi@email.com", "Jakarta")
-user3 = User("Siti", "Rahma", 19, "siti@email.com", "Bandung")
 
-user1.describe_user()
-user1.greet_user()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
+user1.increment_login_attempts()
 
-user2.describe_user()
-user2.greet_user()
+print("Jumlah login attempts:", user1.login_attempts)
 
-user3.describe_user()
-user3.greet_user()
+user1.reset_login_attempts()
+
+print("Login attempts setelah reset:", user1.login_attempts)
+
+#user1 = User("Muhamad", "Irfandi", 20, "irfandi@email.com", "Pekanbaru")
+#user2 = User("Andi", "Saputra", 22, "andi@email.com", "Jakarta")
+#user3 = User("Siti", "Rahma", 19, "siti@email.com", "Bandung")
+
+#user1.describe_user()
+#user1.greet_user()
+
+#user2.describe_user()
+#user2.greet_user()
+
+#user3.describe_user()
+#user3.greet_user()
--- PBO/Tugas PBO.py
+++ PBO/Tugas PBO.py
@@ -1,7 +1,8 @@
 class Restaurant:
-    def __init__(self,restaurant_name,Cuisine_type):
+    def __init__(self,restaurant_name,Cuisine_type,):
         self.restaurant_name = restaurant_name
         self.Cuicine_type = Cuisine_type
+        self.number_served = 0
 
     def describe_restaurant(self):
         print("nama restaurant :", self.restaurant_name)
@@ -10,10 +11,28 @@ class Restaurant:
     def open_restaurant(self):
         print(self.restaurant_name, "sekarang buka")
 
+    def set_number_served(self, number):
+        self.number_served = number
+
+    def increment_number_served(self, additional):
+        self.number_served += additional
+
 restaurant = Restaurant("Restaurant Nusantara", "Masakan Indonesia")
-
-print("Nama restaurant :", restaurant.restaurant_name)
-print("Jenis Masakan :", restaurant.Cuicine_type)
-
 restaurant.describe_restaurant()
-restaurant.open_restaurant()
+
+print("Jumlah pelanggan dilayani:", restaurant.number_served)
+restaurant.number_served =15
+print("Jumlah pelanggan sekarang:", restaurant.number_served)
+restaurant.set_number_served(35)
+print("Jumlah pelanggan setelah diubah:", restaurant.number_served)
+restaurant.increment_number_served(15)
+print("Jumlah pelanggan setelah penambahan:", restaurant.number_served)
+
+
+#print("Nama restaurant :", restaurant.restaurant_name)
+#print("Jenis Masakan :", restaurant.Cuicine_type)
+
+#restaurant.describe_restaurant()
+#restaurant.open_restaurant()
+#restaurant.set_number_served(15)
+#restaurant.increment_number_served(20)
